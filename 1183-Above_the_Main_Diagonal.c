d0f8d252de83854ad8508a8ea7e0370fca1ea575
fardin36 <thunder.fardin@gmail.com>
2023-12-15 21:02:12 +0600

    headache

---
+++ 1183-Above_the_Main_Diagonal.c
@@ -0,0 +1,27 @@
+// Problem 1183 - Above the Main Diagonal
+
+#include <stdio.h>
+int main()
+{
+    char c;
+    int x = 0;
+    scanf(" %c", &c);
+    double m, s = 0.0;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%lf", &m);
+            if (j > i)
+            {
+                s += m;
+                x++;
+            }
+        }
+    }
+    if (c == 'S')
+        printf("%.1lf\n", s);
+    if (c == 'M')
+        printf("%.1lf\n", s / x);
+    return 0;
+}
---
+++ 1190-Right_Area.c
@@ -0,0 +1,27 @@
+// Problem 1190 - Right Area
+
+#include <stdio.h>
+int main()
+{
+    char c;
+    int x = 0;
+    scanf(" %c", &c);
+    double m, s = 0.0;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%lf", &m);
+            if (i < j && i > 11 - j)
+            {
+                s += m;
+                x++;
+            }
+        }
+    }
+    if (c == 'S')
+        printf("%.1lf\n", s);
+    if (c == 'M')
+        printf("%.1lf\n", s / x);
+    return 0;
+}
